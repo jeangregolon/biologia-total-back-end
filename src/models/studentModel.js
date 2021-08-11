8aca99b4a0e4357686a0c377caa69b8326914fc9
jeangregolon <jean.gregolon@gmail.com>
2021-08-11 20:38:19 -0300

    Created student model

---
+++ src/models/studentModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Student = mongoose.model("Student", StudentSchema);
+
+module.exports(Student);
