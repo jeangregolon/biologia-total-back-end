383d8a6cd0c29a2750dd6131117f23f4520d43c5
jeangregolon <jean.gregolon@gmail.com>
2021-08-11 16:31:28 -0300

    Added mongoose to project

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+  res.send('Server is ready')
+})
+
 app.listen(port, () => {
   console.log(`Listen on port ${port}`)
 })
