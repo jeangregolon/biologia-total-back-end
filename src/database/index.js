74fbc6da0b1223df1b9fb66d87301c5ec7cda79e
jeangregolon <jean.gregolon@gmail.com>
2021-08-13 14:40:46 -0300

    Changed DB connection address

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/biologia-total', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-})
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://127.0.0.1/biologia-total",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
-module.exports = mongoose
+module.exports = mongoose;
