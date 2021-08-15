b490f0c56760ca53a17e07ff8bad132bcfa9995a
jeangregolon <jean.gregolon@gmail.com>
2021-08-14 23:09:34 -0300

    Added dotenv package and files

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
+
 const express = require("express");
 
 const app = express();
@@ -8,7 +12,7 @@ require("./controllers/studentController")(app);
 require("./controllers/courseController")(app);
 
 app.get("/", (req, res) => {
-  res.send("Server is ready");
+  res.status(200).send("Server is ready");
 });
 
 module.exports = app;
